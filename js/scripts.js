9c12abee14229be67411e56485bda493cbb73e99
martyminion <martkimwaweru@gmail.com>
2020-02-14 16:04:52 +0300

    Adds styles to the sides of the page

--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,15 @@
     {
         Day = "Saturday";
     }
-
+    
+    if(DD<0 || DD>31){
+      alert("Please Enter a valid day")
+    }
+    if(MM<0 || MM>12){
+      alert("Please Enter a valid Month")
+    }
+    if(YY)
+    
     if (document.getElementById('gendm').checked) {
       document.getElementById("akan").innerText = "Your Akan name is "+malenames[siku]+" because you were born on " +Day
       
